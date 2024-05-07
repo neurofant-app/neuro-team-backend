9e864fb2dfc84d54e7ad367f1fe74a1d8fd6fbbe
Oswaldo Guzman <oswaldo.guzman@staples.ca>
2024-05-06 22:50:31 -0600

    RevisionAPIGenerica

---
+++ src/pod/controlescolar/controlescolar.modelo/alumnos/CreaAlumno.cs
@@ -0,0 +1,21 @@
+﻿using comunes.primitivas.atributos;
+
+namespace controlescolar.modelo.alumnos;
+
+/// <summary>
+/// DTO de creación para un alumno perteneciente a un campus
+/// </summary>
+[CQRSCrear]
+public class CreaAlumno: PersonaBase
+{
+
+    /// <summary>
+    /// Identificador único del campus al que pertenece el alumno
+    /// </summary>
+    public virtual required Guid CampusId { get; set; }
+
+    /// <summary>
+    /// IDentificadpr único del alumno dentro del campus por ejemplo número de alumno 
+    /// </summary>
+    public string? IdInterno { get; set; }
+}
