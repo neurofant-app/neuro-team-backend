0ecc4c0e141de0bf70e927ed2272f6c586b608e2
Oswaldo Guzman <oswaldo.guzman@staples.ca>
2024-06-10 09:25:57 -0600

    EndpointsControlAcceso

---
+++ src/pod/seguridad/seguridad.api/Controllers/ControlAccesoController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.PortableExecutable;
+using apigenerica.primitivas;
+using seguridad.modelo;
+using System.ComponentModel.DataAnnotations;
+
+namespace seguridad.api.Controllers;
+
+[Route("controlacceso")]
+[ApiController]
+public class ControlAccesoController : ControladorBaseGenerico
+{
+
+    private ILogger<ControlAccesoController> logger;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="logger"></param>
+    /// <param name="contextAccessor"></param>
+    public ControlAccesoController(ILogger<ControlAccesoController> logger, HttpContextAccessor contextAccessor ): base(contextAccessor ) {
+        this.logger = logger;
+    }
+
+
+    [HttpGet("interno/roles/{usuarioId}")]
+    public async Task<ActionResult<List<Rol>>> ObtieneRolesUsuarioInterno([Required]string usuarioId, [FromHeader(Name = DOMINIOHEADER)] string dominioId, [FromHeader(Name = UORGHEADER)] string uOrgID)
+    {
+        // DEbe devolver 
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("interno/permisos/{aplicacionId}/{usuarioId}")]
+    public async Task<ActionResult<List<Permiso>>> ObtienePermisosAplicacionInterno([Required] string aplicacionId, [Required] string usuarioId, [FromHeader(Name = DOMINIOHEADER)] string dominioId, [FromHeader(Name = UORGHEADER)] string uOrgID)
+    {
+        // DEbe devolver 
+        throw new NotImplementedException();
+    }
+
+}
