5cddb99cfa19d6d1849e8bffd82cab6167cbe069
desarrollo <mediaob11@gmail.com>
2024-08-06 11:44:05 -0600

    Conversacion Models

---
+++ src/pod/conversaciones/comunicaciones.model/Plantilla/Plantilla.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace comunicaciones.model;
+/// <summary>
+/// Plantilla para envio de mensaje
+/// </summary>
+public class Plantilla
+{
+    /// <summary>
+    /// Identificador único de la plantilla puede ser autogenerado o definido
+    /// como un conjunto de valores.
+    /// </summary>
+    [BsonId]
+    public string Id { get; set; }
+    [BsonElement("lc")]
+    public List<Contenido> Contenidos { get; set; }
+    /// <summary>
+    /// Identificador de la aplicación  que utiliza la plantilla
+    /// </summary>
+    [BsonElement("aid")]
+    public string? AplicacionId { get; set; }
+    /// <summary>
+    /// Es una plantilla utilizada por el usuario
+    /// </summary>
+    [BsonElement("du")]
+    public bool DeUsuario { get; set; }
+    /// <summary>
+    /// Usuario creado por la plantilla
+    /// </summary>
+    [BsonElement("uid")]
+    public string? UsuarioId { get; set; }
+    [BsonElement("fc")]
+    public DateTime FechaCreacion { get; set; }
+    [BsonElement("fe")]
+    public DateTime? FechaEnvio { get; set; }
+}
---
+++ src/pod/conversaciones/comunicaciones.model/PrepagoDisponible/PrepagoDisponible.cs
@@ -0,0 +1,18 @@
+﻿
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace comunicaciones.model;
+
+public class PrepagoDisponible
+{
+    [BsonId]
+    public string PrepagoId { get; set; }
+    [BsonElement("eid")]
+    public string EmisorId { get; set; }
+    [BsonElement("c")]
+    public TipoCanal Canal { get; set; }
+    [BsonElement("mr")]
+    public long MensajesRestantes { get; set; }
+    [BsonElement("br")]
+    public long BytesRestantes { get; set; }
+}
