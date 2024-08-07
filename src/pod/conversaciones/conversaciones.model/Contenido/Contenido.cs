ab9d7d0f634a70a700baafa52cd5fafb0d16988a
desarrollo <mediaob11@gmail.com>
2024-08-07 08:59:09 -0600

    modelo

--- src/pod/conversaciones/conversaciones.model/Contenido/Contenido.cs
+++ src/pod/conversaciones/conversaciones.model/Contenido/Contenido.cs
@@ -6,7 +6,7 @@ public class Contenido
 {
     [BsonId]
     public string Id { get; set; }
-    [BsonElement("tc")]
+    [BsonElement("t")]
     public TipoCanal Canal { get; set; }
     [BsonElement("c")]
     public string Cuerpo { get; set; }
