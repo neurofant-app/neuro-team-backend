eb313f1a1cf9c90fe5fc3fd5766e75e5d6c75d04
OSwaldo Diaz <odiaz@neurofant.com>
2025-03-19 18:50:49 -0600

    ExtensionesJsonGenericas

--- src/lib/apigenerica.model/servicios/Extensiones.cs
+++ src/lib/apigenerica.model/servicios/Extensiones.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.Json;
+﻿using apigenerica.model.modelos;
+using comunes.primitivas;
+using System.Text.Json;
 
 namespace apigenerica.model.servicios
 {
@@ -6,9 +8,24 @@ namespace apigenerica.model.servicios
     {
         public static T ReserializeCamelCase<T>(this object o)
         {
-            var options = new JsonSerializerOptions() { PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase };
+            var options = new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
             string s = JsonSerializer.Serialize(o, options);
             return JsonSerializer.Deserialize<T>(s, options)!;
+        }
+
+
+        public static RespuestaPayload<object> ReserializePayloadCamelCase(this object o)
+        {
+            var options = new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            string s = JsonSerializer.Serialize(o, options);
+            return JsonSerializer.Deserialize<RespuestaPayload<object>>(s, options)!;
+        }
+
+        public static RespuestaPayload<PaginaGenerica<object>> ReserializePaginaCamelCase(this object o)
+        {
+            var options = new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            string s = JsonSerializer.Serialize(o, options);
+            return JsonSerializer.Deserialize<RespuestaPayload<PaginaGenerica<object>>>(s, options)!;
         }
     }
 }
